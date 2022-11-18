5c289be7c293ac887fb895b2a5416fb6aa89a1a5
Andre Luis <dedefest@gmail.com>
2022-11-18 18:38:50 -0300

    configarao inicial de cors e api para o projeto

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Bem vindo a esta API',
+        'version' => '1.0.0',
+        'success' => true
+    ]);
 });
+
